7ff478921b9643c242e6a395a16d26376a3ebb26
Fatih <yildizafatih@gmail.com>
2024-07-24 12:42:45 +0300

    port change to .env

--- server/app.js
+++ server/app.js
@@ -30,5 +30,6 @@ app.use("/messages", messageRoutes);
 app.use("/openai", openaiRoutes);
 
 // Start the server
-const PORT = 5001;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server running on port ${process.env.PORT}`)
+);
